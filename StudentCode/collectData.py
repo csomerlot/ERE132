03708404e605a296aac98cdd07882d4fb824f26e
Chris Somerlot <csomerlot@gmail.com>
2016-10-11 09:35:13 -0400

    Added meta comments

--- StudentCode/collectData.py
+++ StudentCode/collectData.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
+
+## Import libraries to help minmize the code you need
 from logger import *
 from time import sleep
 
 ##from mcp3008 import *  ### swap these libraries once RPi is wired with sensors
-from virtual import *
+from virtual import *    ### this is the library used for testing you code, on an RPi or elsewhere
 
+## These constants define the communication pins of the GPIO on the RPi
 # change these as needed (change the Nones)
 SPICLK = None
 SPIMISO = None
 SPIMOSI = None
 SPICS = None
 
+## These setup function calls tell the RPi which pins are for input, which are for output
 # set up the Serial Peripheral Interface (SPI) pins (change the Nones)
 GPIO.setup (None, GPIO.OUT)
 GPIO.setup (None, GPIO.IN)
 GPIO.setup (None, GPIO.OUT)
 GPIO.setup (None, GPIO.OUT)
 
+## these log calls set up your log file so taht you can tell what data gets collected
 log()
 log("temp", "light")
 
 while True: ## infinite loop
-    
+
+    ##  These read calls get a digital value from the ADC chip connected to the RPi's GPIO pins 
     lightReading = readadc(0, SPICLK, SPIMOSI, SPIMISO, SPICS)
     tempReading  = readadc(1, SPICLK, SPIMOSI, SPIMISO, SPICS)
 
