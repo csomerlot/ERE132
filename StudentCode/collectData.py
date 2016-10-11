9b738d9702ea9e944b43c3c47533b9a65dde28cc
Chris Somerlot <csomerlot@brwncald.com>
2016-10-10 21:25:14 -0400

    debug and finalize for deployment

--- StudentCode/collectData.py
+++ StudentCode/collectData.py
@@ -2,34 +2,35 @@
 from logger import *
 from time import sleep
 
-##from mcp3008 import *
+##from mcp3008 import *  ### swap these libraries once RPi is wired with sensors
 from virtual import *
 
-# change these as desired
+# change these as needed (change the Nones)
 SPICLK = None
 SPIMISO = None
 SPIMOSI = None
 SPICS = None
-Secs = 0
 
-# set up the Serial Peripheral Interface (SPI) pins
+# set up the Serial Peripheral Interface (SPI) pins (change the Nones)
 GPIO.setup (None, GPIO.OUT)
 GPIO.setup (None, GPIO.IN)
 GPIO.setup (None, GPIO.OUT)
 GPIO.setup (None, GPIO.OUT)
 
+log()
+log("temp", "light")
 
 while True: ## infinite loop
     
     lightReading = readadc(0, SPICLK, SPIMOSI, SPIMISO, SPICS)
     tempReading  = readadc(1, SPICLK, SPIMOSI, SPIMISO, SPICS)
 
-    #### Do math to convert reading from digital readings to real value
+    #### Do the math to convert reading from digital readings to real value
     temp  = tempReading
     light = lightReading
 
     #### write it out to the data file
     log(temp, light)
     
-    sleep(1)
+    sleep(1) # pauses for 1 second
 
--- StudentCode/logger.py
+++ StudentCode/logger.py
@@ -2,8 +2,10 @@ from datetime import datetime as dt
 
 newline = "\n"
 
-def log(message):
-    print (message)
+def log(*messages):
+    text = ""
+    for m in messages: text += "%s\t" % m
+    print (text)
     with open(__file__+".log", 'a') as logfile:
-        logfile.write("\t%s\t%s" % (dt.now(), message))
+        logfile.write("%s:\t%s\n" % (dt.now(), text))
 
