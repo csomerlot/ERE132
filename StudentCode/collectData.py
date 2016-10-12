3c40db08baa58b3334803a4f1c469ded9bf57dd6
Chris Somerlot <csomerlot@brwncald.com>
2016-10-11 21:54:14 -0400

    Swapped out psuedocode for real code

--- StudentCode/collectData.py
+++ StudentCode/collectData.py
@@ -8,20 +8,18 @@ from time import sleep
 from virtual import *    ### this is the library used for testing you code, on an RPi or elsewhere
 
 ## These constants define the communication pins of the GPIO on the RPi
-# change these as needed (change the Nones)
-SPICLK = None
-SPIMISO = None
-SPIMOSI = None
-SPICS = None
+SPICLK  = 18
+SPIMISO = 23
+SPIMOSI = 24
+SPICS   = 25
 
 ## These setup function calls tell the RPi which pins are for input, which are for output
-# set up the Serial Peripheral Interface (SPI) pins (change the Nones)
-GPIO.setup (None, GPIO.OUT)
-GPIO.setup (None, GPIO.IN)
-GPIO.setup (None, GPIO.OUT)
-GPIO.setup (None, GPIO.OUT)
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
 
-## these log calls set up your log file so taht you can tell what data gets collected
+## these log calls set up your log file so that you can tell what data gets collected
 log()
 log("temp", "light")
 
@@ -32,8 +30,8 @@ while True: ## infinite loop
     tempReading  = readadc(1, SPICLK, SPIMOSI, SPIMISO, SPICS)
 
     #### Do the math to convert reading from digital readings to real value
-    temp  = tempReading
-    light = lightReading
+    tempCelcius  = 100*(3.3 * tempReading / 1024.0) - 50
+    light = 100.0 * lightReading / 1024.0 ## % of full light
 
     #### write it out to the data file
     log(temp, light)
